65c2234206d176c4aae6be14a0ad2d4f1a85b5ac
Kathide Vilakazi <Sidwelljnq@gmail.com>
2023-08-24 00:10:05 +0200

    Added docstrings to user_auth/forms.py

--- user_auth/forms.py
+++ user_auth/forms.py
@@ -4,8 +4,37 @@ from django.contrib.auth.models import User  # Import User model
 
 # Create a custom user creation form by extending UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)  # Add email field
+    """
+    A custom user creation form that extends UserCreationForm and adds an email field.
+
+    This form is used for user registration and includes fields for username, email, and passwords.
+
+    :param UserCreationForm: The base form for user creation provided by Django.
+    :type UserCreationForm: Form class
+    """
+
+    email = forms.EmailField(required=True)
+    """
+    An email field for the user's email address.
+
+    :param EmailField: A field for email addresses.
+    :type EmailField: EmailField
+    :param required: Indicates whether this field is required. True means it's required.
+    :type required: bool
+    """
 
     class Meta:
         model = User
+        """
+        The User model to use for user creation.
+
+        :param User: The User model provided by Django's authentication system.
+        :type User: Model class
+        """
         fields = ('username', 'email', 'password1', 'password2')
+        """
+        The fields to include in the form.
+
+        :param fields: A tuple of field names to include in the form.
+        :type fields: tuple
+        """
