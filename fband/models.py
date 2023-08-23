999b1a4d5a63259b79d4cee219d336202e8a218a
Kathide Vilakazi <Sidwelljnq@gmail.com>
2023-08-23 13:09:02 +0200

    Added docstrings to fband/models.py

--- fband/models.py
+++ fband/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 
 class Album(models.Model):
+    """
+    Model representing an album.
+    """
+    
     # Fields for the Album model
-    title = models.CharField(max_length=200)       # Title of the album
-    description = models.TextField()               # Description of the album
+    
+    title = models.CharField(max_length=200)
+    """
+    The title of the album.
+    """
+    
+    description = models.TextField()
+    """
+    A description of the album.
+    """
+    
     cover_image = models.ImageField(
-        upload_to='album_covers/',                 # Directory to upload cover images
-        default=r'C:/Users/Sidwe/OneDrive/Documents/HyperionDev/Level 2/L2T23/myBand/static/images'
-        )                                           # Default cover image path
-
+        upload_to='album_covers/',
+        default='path_to_default_image.png'
+    )
+    """
+    The cover image of the album.
+    
+    :param upload_to: The directory to upload cover images.
+    :param default: The default cover image path.
+    """
+    
     def __str__(self):
-        return self.title   # String representation of the album object
+        """
+        Return a string representation of the album.
+        """
+        return self.title
